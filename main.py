0b552366808be3dd9b346520c121967750ee23fc
khoai <khanhphuongdo28@gmail.com>
2024-01-27 11:17:44 -0600

    update

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 # Importing Libraries
 import pygame
 import random
-
+from sys import exit
 # Initialize PyGame
 pygame.init()
 
@@ -10,3 +10,5 @@ pygame.init()
 WIDTH = 800
 HEIGHT = 600
 TITLE = "Space Invaders Game"
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption(TITLE)
